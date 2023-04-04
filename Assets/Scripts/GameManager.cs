53709e6fad4f75e2a2ce2e5b76c6a04f2fc75018
joanqueralt94 <salnitre94@gmail.com>
2023-04-04 08:42:36 +0200

    Added Pong Project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+ 
+    public GameObject ball;
+
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player1Goal;
+    public GameObject player2Goal;
+
+    public Text player1text;
+
+    public Text player2text;
+
+    private int player1Score;
+
+    private int player2Score;
+
+    public void Player1Scored()
+    {
+        player1Score++;
+        player1text.text = player1Score.ToString();
+        ResetPosition();
+    }
+
+    public void Player2Scored()
+    {
+        player2Score++;
+        player2text.text = player2Score.ToString();
+        ResetPosition();
+    }
+
+    private void ResetPosition()
+    {
+        ball.GetComponent<Ball>().Reset();
+        player1.GetComponent<Players>().Reset();
+        player2.GetComponent<Players>().Reset();
+    }
+
+
+
+}
---
+++ Assets/Scripts/Players.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Players : MonoBehaviour
+{
+
+    public bool player1;
+    public float speed = 3;
+    public Rigidbody2D rb;
+
+    private float move;
+    private Vector2 startPos;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;  
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player1)
+        {
+            move = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            move = Input.GetAxis("Vertical2");
+        }
+
+        rb.velocity = new Vector2(rb.velocity.x, move*speed);
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = startPos;
+    }
+
+}
